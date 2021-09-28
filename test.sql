43a8d0e6c47662ffe73e9ac41c9cc5112b6dfcc9
jordi <jordi8101@gmail.com>
2021-09-27 20:54:08 -0700

    T8

--- test.sql
+++ test.sql
@@ -1,1 +1,13 @@
-DROP DATABASE ORDERSDB;
+USE ORDERSDB;
+-- EXEC sp_bindrule ZIP_LENGTH, 'CUSTOMER.zip';
+-- EXEC sp_bindrule ZIP_LENGTH, 'ORDERS.zip';
+
+-- EXEC sp_bindrule CUSTOMER_ID_FORMAT, 'CUSTOMER.zip';
+-- EXEC sp_bindrule AREACODE_FORMAT, 'SUPPLIER.zip';
+-- EXEC sp_bindrule AREACODE_FORMAT, 'CUSTOMER.zip';
+-- EXEC sp_bindrule AREACODE_FORMAT, 'ORDERS.zip';
+
+EXEC sp_bindrule NAME_FORMAT, 'SUPPLIER.supName';
+EXEC sp_bindrule NAME_FORMAT, 'CUSTOMER.firstName';
+EXEC sp_bindrule NAME_FORMAT, 'CUSTOMER.lastName';
+
