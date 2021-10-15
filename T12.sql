2e78ded43acc8e544c5b76b62cb3f40d717444a4
jordi <jordi8101@gmail.com>
2021-10-14 20:50:17 -0700

    T12 Views

--- T12.sql
+++ T12.sql
@@ -4,10 +4,16 @@ GO
 -- 1.Despliegue los datos de los clientes, unido a la información de las órdenes que
 -- haya adquirido cada cliente.
 
--- SELECT CUSTOMER.*, ORDERS.*  FROM ORDERCUSTOMER
--- INNER JOIN CUSTOMER ON CUSTOMER.custId = ORDERCUSTOMER.custId
--- INNER JOIN ORDERS ON ORDERCUSTOMER.orderNo = ORDERS.orderNo
--- GO
+SELECT CUSTOMER.*, ORDERS.*  FROM ORDERCUSTOMER
+INNER JOIN CUSTOMER ON CUSTOMER.custId = ORDERCUSTOMER.custId
+INNER JOIN ORDERS ON ORDERCUSTOMER.orderNo = ORDERS.orderNo
+GO
+--? View
+CREATE VIEW CUSTOMER_ORDERS AS
+SELECT CUSTOMER.*, ORDERS.*  FROM ORDERCUSTOMER
+INNER JOIN CUSTOMER ON CUSTOMER.custId = ORDERCUSTOMER.custId
+INNER JOIN ORDERS ON ORDERCUSTOMER.orderNo = ORDERS.orderNo
+GO
 
 -- 2.Despliega los datos de los artículos, junto con la información de las órdenes en la
 -- que se haya efectuado
@@ -16,14 +22,26 @@ GO
 -- INNER JOIN ITEM ON ITEM.itemNo = ORDERITEM.itemNo
 -- INNER JOIN ORDERS ON ORDERITEM.orderNo = ORDERS.orderNo
 -- GO
+--? View
+CREATE VIEW ITEM_ORDERS AS
+SELECT ITEM.*, ORDERS.* FROM ORDERITEM
+INNER JOIN ITEM ON ITEM.itemNo = ORDERITEM.itemNo
+INNER JOIN ORDERS ON ORDERITEM.orderNo = ORDERS.orderNo
+GO
 
 -- 3.Despliega los datos de los proveedores y los datos de los artículos que
 -- ellos surten, incluye el costo unitario
 
--- SELECT SUPPLIER.*, ITEM.* FROM SUPPLERITEM 
--- INNER JOIN SUPPLIER ON SUPPLIER.supplerNo = SUPPLERITEM.supplerNo
--- INNER JOIN ITEM ON ITEM.itemNo = SUPPLERITEM.itemNo
--- GO
+SELECT SUPPLIER.*, ITEM.* FROM SUPPLERITEM 
+INNER JOIN SUPPLIER ON SUPPLIER.supplerNo = SUPPLERITEM.supplerNo
+INNER JOIN ITEM ON ITEM.itemNo = SUPPLERITEM.itemNo
+GO
+--? View
+CREATE VIEW SUPPLIER_ITEM AS
+SELECT SUPPLIER.*, ITEM.* FROM SUPPLERITEM 
+INNER JOIN SUPPLIER ON SUPPLIER.supplerNo = SUPPLERITEM.supplerNo
+INNER JOIN ITEM ON ITEM.itemNo = SUPPLERITEM.itemNo
+GO
 
 -- 4. Realiza un full outer join con las item, contains y order
 
@@ -31,12 +49,24 @@ GO
 -- FULL OUTER JOIN ITEM ON ITEM.itemNo = ORDERITEM.itemNo
 -- FULL OUTER JOIN ORDERS ON ORDERITEM.orderNo = ORDERS.orderNo
 -- GO
+--? View
+CREATE VIEW ORDERITEM_ITEM_ORDERS AS
+SELECT * FROM ORDERITEM 
+FULL OUTER JOIN ITEM ON ITEM.itemNo = ORDERITEM.itemNo
+FULL OUTER JOIN ORDERS ON ORDERITEM.orderNo = ORDERS.orderNo
+GO
 
 -- 5. Efectúa una consulta left outer join con la tabla supplier, supplies
 -- SELECT SUPPLIER.*, ITEM.* FROM SUPPLERITEM
 -- LEFT OUTER JOIN SUPPLIER ON SUPPLIER.supplerNo = SUPPLERITEM.supplerNo
 -- LEFT OUTER JOIN ITEM ON ITEM.itemNo = SUPPLERITEM.itemNo
 -- GO
+--? View
+CREATE VIEW SUPPLIER_ITEM_LEFT AS
+SELECT SUPPLIER.*, ITEM.* FROM SUPPLERITEM
+LEFT OUTER JOIN SUPPLIER ON SUPPLIER.supplerNo = SUPPLERITEM.supplerNo
+LEFT OUTER JOIN ITEM ON ITEM.itemNo = SUPPLERITEM.itemNo
+GO
 
 -- 6.Despliega los datos de las órdenes, el nombre, calle, ciudad,límite de crédito, de
 -- los clientes que haya realizado órdenes
@@ -45,31 +75,59 @@ GO
 -- INNER JOIN ORDERS ON ORDERS.orderNo = ORDERCUSTOMER.orderNo
 -- INNER JOIN CUSTOMER ON CUSTOMER.custId = ORDERCUSTOMER.custId
 -- GO
-
+--? View
+CREATE VIEW ORDERS_CUSTOMER AS
+SELECT ORDERS.street, ORDERS.city, CUSTOMER.creditLimit, CUSTOMER.firstName FROM ORDERCUSTOMER
+INNER JOIN ORDERS ON ORDERS.orderNo = ORDERCUSTOMER.orderNo
+INNER JOIN CUSTOMER ON CUSTOMER.custId = ORDERCUSTOMER.custId
+GO
 
 -- 7.Despliega los datos de los clientes que haya realizado una orden con un monto
 -- mayor a 10,000
 
--- SELECT CUSTOMER.*, ORDERS.* FROM ORDERCUSTOMER
--- INNER JOIN CUSTOMER ON CUSTOMER.custId = ORDERCUSTOMER.custId
--- INNER JOIN ORDERS ON ORDERS.orderNo = ORDERCUSTOMER.orderNo
--- WHERE ORDERS.totalAmount > 10000
--- GO
-
+SELECT CUSTOMER.*, ORDERS.* FROM ORDERCUSTOMER
+INNER JOIN CUSTOMER ON CUSTOMER.custId = ORDERCUSTOMER.custId
+INNER JOIN ORDERS ON ORDERS.orderNo = ORDERCUSTOMER.orderNo
+WHERE ORDERS.totalAmount > 10000
+GO
+--? View
+CREATE VIEW CUSTOMER_ORDERS_AMOUNT AS
+SELECT CUSTOMER.*, ORDERS.* FROM ORDERCUSTOMER
+INNER JOIN CUSTOMER ON CUSTOMER.custId = ORDERCUSTOMER.custId
+INNER JOIN ORDERS ON ORDERS.orderNo = ORDERCUSTOMER.orderNo
+WHERE ORDERS.totalAmount > 10000
+GO
 -- 8. Despliega la información de los artículos, de las órdenes realizadas por el cliente
 -- jimenez hector
-SELECT CUSTOMER.firstName, CUSTOMER.lastName, ITEM.itemName FROM ORDERCUSTOMER
+
+SELECT CUSTOMER.firstName, CUSTOMER.lastName, ITEM.* FROM ORDERCUSTOMER
 INNER JOIN CUSTOMER ON CUSTOMER.custId = ORDERCUSTOMER.custId
 INNER JOIN ORDERS ON ORDERS.orderNo = ORDERCUSTOMER.orderNo
 INNER JOIN ORDERITEM ON ORDERITEM.orderNo = ORDERS.orderNo
 INNER JOIN ITEM ON ITEM.itemNo = ORDERITEM.itemNo
 WHERE CUSTOMER.firstName = 'jimenez' AND CUSTOMER.lastName = 'hector'
 GO
-
+-- ? View
+CREATE VIEW CUSTOMER_ORDERS_ITEM AS
+SELECT CUSTOMER.firstName, CUSTOMER.lastName, ITEM.* FROM ORDERCUSTOMER
+INNER JOIN CUSTOMER ON CUSTOMER.custId = ORDERCUSTOMER.custId
+INNER JOIN ORDERS ON ORDERS.orderNo = ORDERCUSTOMER.orderNo
+INNER JOIN ORDERITEM ON ORDERITEM.orderNo = ORDERS.orderNo
+INNER JOIN ITEM ON ITEM.itemNo = ORDERITEM.itemNo
+WHERE CUSTOMER.firstName = 'jimenez' AND CUSTOMER.lastName = 'hector'
+GO
 -- 9. Despliega los datos de los productos cuyo precio sea menor que el promedio de
 -- precio
--- SELECT * FROM ITEM WHERE unitPrice < (SELECT AVG(unitPrice) FROM ITEM)
--- GO
+SELECT * FROM ITEM WHERE unitPrice < (SELECT AVG(unitPrice) FROM ITEM)
+GO
+-- ? View
+CREATE VIEW ITEM_AVG AS
+SELECT * FROM ITEM WHERE unitPrice < (SELECT AVG(unitPrice) FROM ITEM)
+GO
 -- 10. Despliega los datos de los productos en cuyo nombre contenga la sílaba jet
--- SELECT * FROM ITEM WHERE itemName LIKE '%jet%'
--- GO
+SELECT * FROM ITEM WHERE itemName LIKE '%jet%'
+GO
+-- ? View
+CREATE VIEW ITEM_JET AS
+SELECT * FROM ITEM WHERE itemName LIKE '%jet%'
+GO
--- T8Constraints.sql
+++ T8Constraints.sql
@@ -1,4 +1,5 @@
-USE ORDERSDB;
+USE ORDERSDB
+GO
 -- 1.Mediante restricción crea las llaves primarias compuestas y llaves foráneas de las tablas Supplies y la tabla Contains.
 -- ALTER TABLE SUPPLIER ADD CONSTRAINT PK_SUPPLIER PRIMARY KEY(supplerNo);
 -- ALTER TABLE CUSTOMER ADD CONSTRAINT PK_CUSTOMER PRIMARY KEY(custId);
@@ -15,3 +16,18 @@ USE ORDERSDB;
 -- ALTER TABLE SUPPLERITEM ADD CONSTRAINT FK_SUPPLIERITEM_ITEM FOREIGN KEY(itemNo) REFERENCES ITEM(itemNo);
 -- ALTER TABLE SUPPLERITEM ADD CONSTRAINT FK_SUPPLIERITEM_SUPPLIER FOREIGN KEY(supplerNo) REFERENCES SUPPLIER(supplerNo);
 
+INSERT INTO CUSTOMER VALUES
+(1, 'Luis', 'Antonio', 123,123456789, 'street 1','city 1', 'state 1', '12345', 1500.0000),
+(2, 'Juan', 'Antonio', 123,123456789, 'street 2','city 2', 'state 2', '12345', 1500.0000),
+(3, 'Pedro', 'Antonio', 123,123456789, 'street 3','city 3', 'state 3', '12345', 1500.0000),
+(4, 'Maria', 'Antonio', 123,123456789, 'street 4','city 4', 'state 4', '12345', 1500.0000),
+(5, 'Luis', 'Antonio', 123,123456789, 'street 5','city 5', 'state 5', '12345', 1500.0000),
+(6, 'Juan', 'Antonio', 123,123456789, 'street 6','city 6', 'state 6', '12345', 1500.0000),
+(7, 'Pedro', 'Antonio', 123,123456789, 'street 7','city 7', 'state 7', '12345', 1500.0000),
+(8, 'Maria', 'Antonio', 123,123456789, 'street 8','city 8', 'state 8', '12345', 1500.0000),
+(9, 'Luis', 'Antonio', 123,123456789, 'street 9','city 9', 'state 9', '12345', 1500.0000),
+(10, 'Juan', 'Antonio', 123,123456789, 'street 10','city 10', 'state 10', '12345', 1500.0000),
+(11, 'Pedro', 'Antonio', 123,123456789, 'street 11','city 11', 'state 11', '12345', 1500.0000),
+(12, 'Maria', 'Antonio', 123,123456789, 'street 12','city 12', 'state 12', '12345', 1500.0000),
+(13, 'Luis', 'Antonio', 123,123456789, 'street 13','city 13', 'state 13', '12345', 1500.0000),
+(14, 'Juan', 'Antonio', 123,123456789, 'street 14','city 14', 'state 14', '12345', 1500.0000);
